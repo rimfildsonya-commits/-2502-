5599532bfaa6fef25d6df2bd693f797a9ef8b788
Васильева <savasileva_1@edu.hse.ru>
2026-06-02 23:31:12 -0700

    Импортирован класс DietaryRecipe(рецепт диеты наследник от Recipe): атрибуты diet_type(тип диеты или же раздела). методы scale (наследует супером scale и возращает новый рецепт), __str__ (приводит в человекочитаемый вид)

--- dietary_recipes.py
+++ dietary_recipes.py
@@ -1,32 +1,11 @@
 from ingredients import Ingredient
 from recipes import Recipe
-class Recipe:
-    def __init__(self, title, ingredients):
-        self.title = title
-        self.ingredients = ingredients
-    def add_ingredient(self, ingredient: Ingredient):
-        for x in self.ingredients:
-            if x.name == ingredient.name and x.unit == ingredient.unit:
-                x.quantity+=ingredient.quantity
-                return
-        self.ingredients.append(ingredient)    
-    @staticmethod
-    def is_valid_ratio(ratio):
-        if type(ratio) == int or type(ratio) == float:
-            if ratio>0:
-                return True
-            else:
-                return False
-    def scale(self, ratio: float):
-        ingred = []
-        for x in self.ingredients:
-            cop = Ingredient(x.name, x.quantity * ratio, x.unit)
-            ingred.append(cop)
-        return(self.title, ingred)
-    def __len__(self):
-        return len(self.ingredients)+1
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients):
+        self.diet_type = diet_type
+        super().__init__(title, ingredients)
+    def scale(self, ratio:float):
+        new_scale = super().scale(ratio)
+        return DietaryRecipe(new_scale.title, self.diet_type, new_scale.ingredients)
     def __str__(self):
-        res = self.title + ': '
-        for x in self.ingredients:
-            res  = res + ' ' + x.name + '(' + str(x.quantity)+ x.unit+'.'+','
-        return res[:-1]
+        return f"[{self.diet_type}]{self.title}: {self.ingredients}"
