5bfdbf8bd0652d65592202f818e34033e7e047c3
Васильева <savasileva_1@edu.hse.ru>
2026-06-03 22:26:24 -0700

    Исправлен класс ShoppingList: добавлен __init__ с items=None, исправлены опечатки self.items, исправлен __add__

--- ingredients.py
+++ ingredients.py
@@ -1,21 +1,25 @@
-class Ingredient:
-    def __init__(self, name, quantity, unit):
-        self.name = name
-        self.quanity = quantity
-        self.unit = unit
-    @property
-    def quantity(self):
-        return self._quantity
-    @quantity.setter
-    def quantity(self, value):
-        if value <= 0:
-            raise ValueError(
-                "Количество должно быть положительным"
-                )
-        self._quantity = float(value)
+from ingredients import Ingredient
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+    def add_ingredient(self, ingredient: Ingredient):
+        for x in self.ingredients:
+            if x.name == ingredient.name and x.unit == ingredient.unit:
+                x.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным числом")
+        new_ingredients = []
+        for x in self.ingredients:
+            new_ingredients.append(Ingredient(x.name, x.quantity * ratio, x.unit))
+        return Recipe(self.title, new_ingredients)
+    def __len__(self):
+        return len(self.ingredients)
     def __str__(self):
-        return f"{self.name}: {self.quantity} {self.unit}"
-    def __repr__(self):
-        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
-    def __eq__(self, other):
-        return self.name == other.name and self.unit == other.unitы
+        return f"{self.title}: {self.ingredients}"
--- shopping_list.py
+++ shopping_list.py
@@ -2,35 +2,37 @@ from ingredients import Ingredient
 from recipes import Recipe
 from dietary_recipes import DietaryRecipe
 class ShoppingList:
-    def __init__(self, items):
+    def __init__(self, items=None):
+        if items is None:
+            items = []
         self.items = items
     def add_recipe(self, recipe: Recipe, portions: float):
-        if portions<=0:
-            raise ValueError(
-                "Количество порций должно быть положительным"
-            )
+        if portions <= 0:
+            raise ValueError("Количество порций должно быть положительным")
         rec = recipe.scale(portions)
         for ingredient in rec.ingredients:
             self.items.append((ingredient, recipe.title))
-    def remove_recipe(self, title:str):
+    def remove_recipe(self, title: str):
         new = []
-        for x in self.item:
-            if x[1]!=title:
+        for x in self.items:
+            if x[1] != title:
                 new.append(x)
-        self.item = new
+        self.items = new
     def get_list(self):
         res = {}
         for x in self.items:
             ingredient = x[0]
-            b = (ingredient.name, ingredient.unit)
-            if b in res:
-                res[b]+= ingredient.quantity
+            key = (ingredient.name, ingredient.unit)
+            if key in res:
+                res[key] += ingredient.quantity
             else:
-                res[b] = ingredient.quantity
-        res2 = []
-        for b, c in res.items:
-            res2.append(Ingredient(b[0], c, b[1]))
-        res2.sort(key=lambda x: x.name)
-        return res2
-    def __add__(self, ther:ShoppingList):
-        return (self.item + other.item)
+                res[key] = ingredient.quantity
+        result = []
+        for key, quantity in res.items():
+            result.append(Ingredient(key[0], quantity, key[1]))
+        result.sort(key=lambda x: x.name)
+        return result
+    def __add__(self, other):
+        new_list = ShoppingList()
+        new_list.items = self.items.copy() + other.items.copy()
+        return new_list
